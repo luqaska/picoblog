cb4ab42f55199b18ddcdb550fbe194ccdf984a50
Lucas <83518257+luqaska@users.noreply.github.com>
2021-09-12 18:02:07 -0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -5,4 +5,8 @@ $nick = "luqaska";
 $avatar = "";
 $description = "Q?";
 $ubication = "";
-$website = ""; // Please! Don't put the protocol (https:// | http://) ?>
+$website = ""; // Please! Don't put the protocol (https:// | http://)
+
+
+// Defaulters
+if($name==false){$name=$nick;}if($avatar==false){$avatar='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👤</text></svg>';} ?>
