a77b11fb9bed126c5acae23a9d06b28505c83fcf
Lucas <83518257+luqaska@users.noreply.github.com>
2021-09-12 15:36:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,23 +16,34 @@ $entries = ($query) ? $mb->getEntries($query) : $mb->getEntries('all');
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $nick ?>@<?= $_SERVER['HTTP_HOST'] ?></title>
-    <link rel="icon" href="<?php echo $avatar ?>">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo($nick) ?>@<?= $_SERVER['HTTP_HOST'] ?>)</title>
+  <link rel="icon" href="<?php echo $avatar ?>">
+  <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
 <body>
-    <h1>picoblog</h1>
-    <?php
-    // Display message and link to main list if viewing a filtered entry list
-    if ($query!=false) {
-        echo '<div>Currently viewing ' . implode('', $query) . '. Back to <a href="' . $_SERVER['PHP_SELF'] . '">list?</a></div>';
-        echo($mb->renderEntries($entries, true, '{entry}'));
-    } else {
-      echo($mb->renderEntries($entries, false, '{entry}'));
+  <a href="<?= $_SERVER['PHP_SELF'] ?>"><header>
+    <h1><img src="<?= $avatar ?>" alt="Avatar"> <?= $name ?> <span id="nick" style="font-weight:normal;font-size:50%">(<?php echo $nick ?>@<?php echo $_SERVER['HTTP_HOST'] ?>)</span></h1>
+  </header></a>
+  <div id="subh"><br>
+    <desc><?= $description ?><?php echo("<a style='color:#e0e0e0;cursor:default'>.</a><a href='$url'><button id='follow'>Follow</button></a>"); ?></desc>
+    <p><?php if($ubication!=false){
+      echo("<span><i class='fa fa-map-marker'></i> $ubication</span>");
     } ?>
+    <?php if($website!=false){
+      echo("<span><i class='fa fa-globe'></i> <a href='http://$website'>$website</a></span>");
+    } ?></p>
+  </div><br>
+  <?php
+  // Display message and link to main list if viewing a filtered entry list
+  if ($query!=false) {
+    //echo '<div>Currently viewing ' . implode('', $query) . '. Back to <a href="' . $_SERVER['PHP_SELF'] . '">list?</a></div>';
+    echo($mb->renderEntries($entries, true, '{entry}'));
+  } else {
+    echo($mb->renderEntries($entries, false, '{entry}'));
+  } ?>
 </body>
 
 </html>
