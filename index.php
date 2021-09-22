bf1652736055e3999b374e3b9788b2f60559956f
Lucas Orleux <83518257+luqaska@users.noreply.github.com>
2021-09-22 14:17:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $entries = ($query) ? $mb->getEntries($query) : $mb->getEntries('all');
     <h1><img src="<?= $avatar ?>" alt="Avatar"> <?= $name ?> <span id="nick" style="font-weight:normal;font-size:50%"><?php echo "({$nick}@{$_SERVER['HTTP_HOST']})" ?></span></h1>
   </header></a>
   <div id="subh"><br>
-    <desc><?= $description ?><?php echo("<a style='color:#e0e0e0;cursor:default'>.</a><a href='$url'><button id='follow'>Follow</button></a>"); ?></desc>
+    <desc><?= $description ?><?php echo("<a style='color:transparent;cursor:default'>.</a><a href='$url'><button id='follow'>Follow</button></a>"); ?></desc>
     <p><?php if($ubication!=false){
       echo("<span><i class='fa fa-map-marker'></i> $ubication</span>");
     } ?>
