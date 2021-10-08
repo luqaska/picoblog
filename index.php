1e46f0ab3d2490957e6b57793c9be275f25277c1
Lucas Orleux <83518257+luqaska@users.noreply.github.com>
2021-10-08 18:13:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $mb = new \hxii\PicoBlog($url);
 $query = $mb->parseQuery();
 $entries = ($query) ? $mb->getEntries($query) : $mb->getEntries('all'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
