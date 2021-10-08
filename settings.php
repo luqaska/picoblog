3fac17c9396c9149030fa7ad562ef41403cca5cf
Lucas Orleux <83518257+luqaska@users.noreply.github.com>
2021-10-08 18:39:37 -0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -7,6 +7,11 @@ $description = "Q?";
 $ubication = "";
 $website = ""; // Please! Don't put the protocol (https:// | http://)
 
+// Utterances (using og:title)
+$utterances=false;
+$utterances_repo="owner/repo";
+$utterances_label="label"; //Leave this in blank if you don't need it
+$utterances_theme="github-dark";
 
 // Defaulters
 if($name==false){$name=$nick;}if($avatar==false){$avatar='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👤</text></svg>';} ?>
