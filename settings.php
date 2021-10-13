033ef7d388d0ec52cec6e857fdba83655a95e458
luqaska <83518257+luqaska@users.noreply.github.com>
2021-10-13 19:06:30 -0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -8,10 +8,15 @@ $ubication = "";
 $website = ""; // Please! Don't put the protocol (https:// | http://)
 
 // Utterances (using og:title)
-$utterances=false;
-$utterances_repo="owner/repo";
-$utterances_label="label"; //Leave this in blank if you don't need it
-$utterances_theme="github-dark";
+$utterances = false;
+$utterances_repo = "owner/repo";
+$utterances_label = "label"; //Leave this in blank if you don't need it
+$utterances_theme = "github-dark";
+
+// Custom CSS
+$css = '
+/* Put your CSS here */
+';
 
 // Defaulters
 if($name==false){$name=$nick;}if($avatar==false){$avatar='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👤</text></svg>';} ?>
